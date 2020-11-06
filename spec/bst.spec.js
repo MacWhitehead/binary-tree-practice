31f031661f756bf395e251e045278720d892a266
MacWhitehead <mackayla.whitehead7485@stu.mtec.edu>
2020-11-06 10:50:45 -0700

    Added in class exercise

---
+++ spec/bst.spec.js
@@ -0,0 +1,181 @@
+describe('BST', function () {
+  var bst;
+  beforeEach(function () {
+    bst = new BinarySearchTree();
+  });
+  describe('insert', () => {
+    it('should insert element with key 8 correctly', function () {
+      bst.insert({
+        key: 8,
+        data: {
+          name: 'number',
+        },
+      });
+      expect(bst.find(8)).toBe(true);
+    });
+    it('should insert elements with key 8, 10, 3 correctly', function () {
+      bst.insert({
+        key: 8,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 10,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 3,
+        data: {
+          name: 'number',
+        },
+      });
+      expect(bst.find(8)).toBe(true);
+      expect(bst.find(10)).toBe(true);
+      expect(bst.find(3)).toBe(true);
+    });
+    it('should insert elements with key 8, 10, 3, 1, 6, 14 correctly', function () {
+      bst.insert({
+        key: 8,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 10,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 3,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 1,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 6,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 14,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 13,
+        data: {
+          name: 'number',
+        },
+      });
+      expect(bst.find(8)).toBe(true);
+      expect(bst.find(10)).toBe(true);
+      expect(bst.find(3)).toBe(true);
+      expect(bst.find(1)).toBe(true);
+      expect(bst.find(6)).toBe(true);
+      expect(bst.find(14)).toBe(true);
+      expect(bst.find(13)).toBe(true);
+    });
+  });
+
+  describe('find', () => {
+    it('should find element with key 8 correctly', function () {
+      bst.insert({
+        key: 8,
+        data: {
+          name: 'number',
+        },
+      });
+      expect(bst.find(8)).toBe(true);
+    });
+    it('should return false if the tree is empty', function () {
+      expect(bst.find(8)).toBe(false);
+    });
+    it('should find elements with key 8, 10, 3, 1, 6, 14 correctly', function () {
+      bst.insert({
+        key: 8,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 10,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 3,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 1,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 6,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 14,
+        data: {
+          name: 'number',
+        },
+      });
+      bst.insert({
+        key: 13,
+        data: {
+          name: 'number',
+        },
+      });
+      expect(bst.find(8)).toBe(true);
+      expect(bst.find(10)).toBe(true);
+      expect(bst.find(3)).toBe(true);
+      expect(bst.find(1)).toBe(true);
+      expect(bst.find(6)).toBe(true);
+      expect(bst.find(14)).toBe(true);
+      expect(bst.find(13)).toBe(true);
+    });
+  it('should return false if key does not exist in tree', function () {
+    bst.insert({
+      key: 8,
+      data: {
+        name: 'number',
+      },
+    });
+    bst.insert({
+      key: 10,
+      data: {
+        name: 'number',
+      },
+    });
+    bst.insert({
+      key: 3,
+      data: {
+        name: 'number',
+      },
+    });
+    expect(bst.find(8)).toBe(true);
+    expect(bst.find(10)).toBe(true);
+    expect(bst.find(3)).toBe(true);
+    expect(bst.find(22)).toBe(false);
+  });
+});
+});
